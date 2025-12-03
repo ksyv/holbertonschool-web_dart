a0bb9cff9215be78be56d16cce13e6e7264df539
ksyv <viais.kevin@hotmail.fr>
2025-12-03 12:16:21 +0100

    add file for task 4

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,55 @@
+class Password {
+  String? _password;
+
+  Password({String? password}) : _password = password;
+
+  set password(String? value) {
+    _password = value;
+  }
+  
+  String? get password => _password;
+
+  bool isValid() {
+    if (_password == null) {
+      return false;
+    }
+
+    final String pwd = _password!;
+    final int length = pwd.length;
+
+    if (length < 8 || length > 16) {
+      return false;
+    }
+
+    bool hasUppercase = false;
+    bool hasLowercase = false;
+    bool hasDigit = false;
+    
+    for (int i = 0; i < length; i++) {
+      final int charCode = pwd.codeUnitAt(i);
+      
+      if (charCode >= 'A'.codeUnitAt(0) && charCode <= 'Z'.codeUnitAt(0)) {
+        hasUppercase = true;
+      } 
+      else if (charCode >= 'a'.codeUnitAt(0) && charCode <= 'z'.codeUnitAt(0)) {
+        hasLowercase = true;
+      } 
+      else if (charCode >= '0'.codeUnitAt(0) && charCode <= '9'.codeUnitAt(0)) {
+        hasDigit = true;
+      }
+      
+      if (hasUppercase && hasLowercase && hasDigit) {
+        break; 
+      }
+    }
+    
+    return hasUppercase && hasLowercase && hasDigit;
+  }
+
+  @override
+  String toString() {
+    final String safePassword = _password ?? 'null';
+    
+    return "Your Password is: $safePassword";
+  }
+}
