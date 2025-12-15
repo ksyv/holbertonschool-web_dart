3fa774f7f3b3212e404768e4cc7b59675f7dd51b
ksyv <ksyviais@gmail.com>
2025-12-15 11:13:04 +0100

    file task 4

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,26 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+    try {
+        final String userJson = await fetchUserData();
+        final Map<String, dynamic> userData = jsonDecode(userJson);
+        final String userId = userData['id'] ?? 'unknown_id';
+
+        final String ordersJson = await fetchUserOrders(userId);
+        final List<dynamic> orders = jsonDecode(ordersJson);
+
+        double totalPrice = 0.0;
+
+        for (String product in orders) {
+            final String priceJson = await fetchProductPrice(product);
+            final num priceAsNum = jsonDecode(priceJson);
+            final double price = priceAsNum.toDouble();
+            totalPrice += price;
+        }
+        return totalPrice;
+
+    } catch (error) {
+        return -1.0;
+    }
+}
